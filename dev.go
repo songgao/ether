4e9e66e58505cab48abeb1395271d55941099d81
Song Gao <song@gao.io>
2014-06-28 21:47:02 -0500

    add GetHardwareAddr(0 in dev.go

--- dev.go
+++ dev.go
@@ -1,8 +1,11 @@
 package ether
 
+import "net"
+
 type Dev interface {
 	Reader() <-chan *FrameWithTime
 	Writer() chan<- *FrameBuf
 	Name() string
 	GetMTU() (int, error)
+	GetHardwareAddr() (net.HardwareAddr, error)
 }
